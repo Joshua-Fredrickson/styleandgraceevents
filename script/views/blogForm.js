eb1224e669aeb95341327bec21018feed8958e5d
Joshua Fredrickson <uafredrickson@gmail.com>
2018-02-12 11:11:47 -0800

    updated

--- script/views/blogForm.js
+++ script/views/blogForm.js
@@ -1,2 +1,9 @@
 'use strict';
 
+article = new Article({
+    author: $('#blog-author').val(),
+    title: $('#blog-title').val(),
+    category: $('#blog-category').val(),
+    body: $('#blog-body').val(),
+    publishedOn: $('#blog-published:checked').length ? new Date() : null
+  });
